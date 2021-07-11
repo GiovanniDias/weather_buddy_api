0f29dfbeaf22eb2b867423af7c3e2fcf468a4cfd
Giovanni Oliveira <giovanni.oliveira@ceuma.br>
2021-07-11 13:59:48 -0300

    chore: Removing TODO comment

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,6 @@ app_name = 'Weather Buddy API'
 def create_app(**config):
     app = Flask(__name__)
 
-    # TODO: Load app settings
     FlaskDynaconf(app, **config)
     
     # TODO: Set api endpoints
