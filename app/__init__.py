1824735776830a5180b73b68fddef0f1edf20f80
Giovanni Oliveira <giovanni.oliveira@ceuma.br>
2021-07-12 15:31:39 -0300

    feat/test: Setting base structure to test endpoints
    - Test each endpoint for success/not found cases
    - Set status_code and response for expected behaviours
    - Add TODO for fetching data from external API.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, make_response, Response, jsonify, request
 from flask_cors import CORS
 from dynaconf import FlaskDynaconf
+from werkzeug.exceptions import InternalServerError
 
 app_name = 'Weather Buddy API'
 
@@ -13,6 +14,60 @@ def create_app(**config):
     @app.get('/')
     def index():
         return app_name
+
+    @app.route('/weather', methods=['GET'])
+    def get_weather_cached_list():
+        args = request.args
+        max_number = args.get('max_number')
+        if max_number is None:
+            max_number = 5
+
+        # TODO: get data from cache
+        # data = rerieve_cached_data()
+        data = []
+
+        try:
+            if data:
+                status_code = 200
+                result = jsonify(data)
+                response = make_response(result, status_code)
+            else:
+                status_code = 404
+                response = Response(status=status_code)
+                response.headers['Content-Type'] = 'application/json'    
+        
+        except InternalServerError:
+            status_code = 500
+            response = Response(status=status_code)
+            response.headers['Content-Type'] = 'application/json'
+        
+        finally:
+            return response
+
+    @app.route('/weather/<city_name>', methods=['GET'])
+    def get_weather_by_city(city_name):
+        
+        # TODO: get weather data of city_name
+        # api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+        data = {}
+
+        try:
+            if data:
+                status_code = 200
+                result = jsonify(data)
+                response = make_response(result, status_code)
+            else:
+                status_code = 404
+                response = Response(status=status_code)
+                response.headers['Content-Type'] = 'application/json'
+        
+        except InternalServerError:
+            status_code = 500
+            response = Response(status=status_code)
+            response.headers['Content-Type'] = 'application/json'
+        
+        finally:
+            return response
 
     CORS(app)
 
