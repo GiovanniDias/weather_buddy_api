603a6ab7b1ecb0457954b5e5cf148cd3f38535da
Giovanni Oliveira <giovanni.oliveira@ceuma.br>
2021-07-13 17:27:53 -0300

    test: Adding test to verify if api is reachable

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,6 +1,9 @@
 import pytest
-# from app.api import get_weather, get_city_weather
+from werkzeug.exceptions import InternalServerError
 
+def test_api_is_reachable(app, client):
+    response = client.get('/')
+    assert response.status_code == 200
 
 @pytest.mark.parametrize('max_number', [None,0,1,10])
 def test_get_weather_cached_list_success(client, max_number):
