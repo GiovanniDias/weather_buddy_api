2f628107377aa96425946e799461a1d0caa922e7
Giovanni Oliveira <giovanni.oliveira@ceuma.br>
2021-07-14 05:12:52 -0300

    hotfix: Setting cache as app property

--- app/extensions/cache.py
+++ app/extensions/cache.py
@@ -2,4 +2,6 @@ from flask_caching import Cache
 
 
 def init_app(app):
-    Cache(app, config={'CACHE_TYPE': 'SimpleCache'})
+    cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache', 'CACHE_DEFAULT_TIMEOUT': 500})
+    app.cache = cache
+
