2d8645a96137bd00f06e1b54f5bf25fe1bc38488
Giovanni Oliveira <giovanni.oliveira@ceuma.br>
2021-07-18 04:01:03 -0300

    feat: Caching and loaded cached data
    - Caching all data successfully retrieved
    - Loading all cached data that aren't a response object

--- app/api/controllers.py
+++ app/api/controllers.py
@@ -1,7 +1,7 @@
-from flask import json, make_response, Response, jsonify
+from flask import make_response, Response, jsonify, current_app
 from werkzeug.exceptions import InternalServerError
 from app.utils import get_status_code
-from ..api.services import get_weather_by_city_service, load_from_cache, save_to_cache
+from ..api.services import get_weather_by_city_service, load_all_from_cache, save_to_cache
 
 class WeatherController:
     def get_weather_by_city(city_name: str) -> Response:
@@ -20,16 +20,16 @@ class WeatherController:
             print(e)
 
     def get_weather_list(args: dict) -> Response:
-        max_number = args.get('max_number')
+        max_number = args.get('max')
         if max_number is None:
-            max_number = 5
+            max_number = current_app.config.MAX_CACHE_ENTRIES
 
         try:
             status_code = 200
-            data = load_from_cache(max_number)
+            data = load_all_from_cache(int(max_number))
             response = make_response(jsonify(data), status_code)
             response.headers['Content-Type'] = 'application/json'    
-            return response
+            return data
         
         except InternalServerError as e:
             print(e)
--- app/api/services.py
+++ app/api/services.py
@@ -1,4 +1,6 @@
 import requests
+from datetime import datetime
+from flask import current_app
 from app.utils import get_api_url
 
 
@@ -8,9 +10,26 @@ def get_weather_by_city_service(city_name: str) -> dict:
     return data
 
 
-def save_to_cache(data: dict) -> None:
-    pass
+def save_to_cache(data: dict, timeout: int = 300) -> None:
+    key = data.get('sys', {}).get('id')
+    data['fetched_at'] = datetime.now()
+    current_app.cache.set(key=key, value=data, timeout=timeout)
 
 
-def load_from_cache(max_number) -> list:
-    return []
+def load_from_cache(key: str) -> list:
+    return current_app.cache.get(key=key)
+    
+
+def load_all_from_cache(max_number) -> list:
+    field = 'fetched_at'
+    cache = current_app.cache
+    
+    keys = list(filter(
+        lambda item: isinstance(cache.get(item), dict),
+        cache.cache._cache
+    ))
+    cached_items = [cache.get(key) for key in keys if key is not None]
+
+    data = {"data": sorted(cached_items, key=lambda x: x[field], reverse=True)[:max_number]}
+        
+    return data
