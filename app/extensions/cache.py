9fed7df504c8632c78c0ca37f8119abb291c5da8
Giovanni Oliveira <giovanni.oliveira@ceuma.br>
2021-07-19 08:09:29 -0300

    hotfix: Using .env file into docker building process

--- app/extensions/cache.py
+++ app/extensions/cache.py
@@ -2,6 +2,6 @@ from flask_caching import Cache
 
 
 def init_app(app):
-    cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache', 'CACHE_DEFAULT_TIMEOUT': 500})
+    cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache', 'CACHE_DEFAULT_TIMEOUT': 300})
     app.cache = cache
 
